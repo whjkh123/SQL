1002d9c03ef11bc5c7bbc397b01c8ec55ca896f5
whjkh <whjkh@DESKTOP-O9T8HTK>
2020-12-18 17:08:22 +0900

    2020. 12. 18 수업과제 #COUNT #MAX/MIN() #TO_CHAR #AVG() #GROUP BY #ORDER BY #ROUND() #HAVING #CASE ~ END #BETWEEN

--- Practice/201218_practice02.sql
+++ Practice/201218_practice02.sql
@@ -22,7 +22,7 @@ FROM    employees;
 마지막으로 신입사원이 들어온 날 출력
 다음 형식으로 출력 ex) 2014년 07월 10일
 */
-SELECT  TO_CHAR(MAX(hire_date),'yyyy"년" mm"월" dd"일"')"마지막 직원 입사일"
+SELECT  TO_CHAR(MAX(hire_date),'yyyy"년" mm"월" dd"일"')"마지막 사원 입사일"
 FROM    employees;
 
 /*
@@ -30,7 +30,7 @@ FROM    employees;
 #1 부서별로 평균임금, 최고임금, 최저임금을 부서아이디(department_id)와 함께 출력
 #2 부서번호(department_id) 내림차순 정렬
 */
-SELECT  department_id,
+SELECT  department_id 부서ID,
         ROUND(AVG(salary),2) 평균임금,
         MAX(salary) 최고임금,
         MIN(salary) 최저임금
@@ -50,7 +50,7 @@ SELECT  job_id,
         MIN(salary) 최저임금
 FROM    employees
 GROUP BY job_id
-ORDER BY MIN(salary) desc, AVG(salary) asc;-- 최저임금이 같을 경우 평균임금 올림차순 정렬
+ORDER BY 최저임금 desc, 평균임금 asc;-- 최저임금이 같을 경우 평균임금 올림차순 정렬, >> ORDER BY MIN(salary) desc, AVG(salary) asc;
 
 /*
 문제 6.
@@ -65,25 +65,25 @@ FROM    employees;
 #1 평균임금과 최저임금의 차이가 2000 미만인 부서(department_id)의 평균임금, 최저임금 그리고 (평균임금 – 최저임금)를 (평균임금 – 최저임금) 출력
 #2 임금편차 내림차순 정렬
 */
-SELECT  department_id,
+SELECT  department_id 부서ID,
         ROUND(AVG(salary),0) 평균임금,
         MIN(salary) 최저임금,
         ROUND(AVG(salary)-MIN(salary),0) 임금편차
 FROM    employees
 GROUP BY department_id
 HAVING  AVG(salary)-MIN(salary) < 2000
-ORDER BY AVG(salary)-MIN(salary) desc;
+ORDER BY 임금편차 desc;-- >> ORDER BY ROUND(AVG(salary)-MIN(salary),0) desc;
 
 /*
 문제 8.
 #1 업무(JOBS)별로 최고임금과 최저임금의 차이 출력
 #2 임금편차 내림차순 정렬
 */
-SELECT  job_id,
+SELECT  job_id 업무,
         MAX(salary)-MIN(salary) 임금편차
 FROM    employees
 GROUP BY job_id
-ORDER BY MAX(salary)-MIN(salary) desc;
+ORDER BY 임금편차 desc;-- >> MAX(salary)-MIN(salary) desc;
 
 /*
 문제 9.
@@ -92,7 +92,7 @@ ORDER BY MAX(salary)-MIN(salary) desc;
 #2 평균급여 내림차순으로 정렬
 #3 평균급여 소수점 첫째짜리에서 반올림 출력
 */
-SELECT  manager_id,
+SELECT  manager_id 매니저ID,
         ROUND(AVG(salary),1) 평균임금,
         MIN(salary) 최소급여,
         MAX(salary) 최대급여
@@ -100,7 +100,7 @@ FROM    employees
 GROUP BY manager_id, hire_date
 HAVING  AVG(salary) >= 5000
         and hire_date >= '05.01.01'
-ORDER BY AVG(salary) desc;
+ORDER BY 평균임금 desc;-- >> ORDER BY ROUND(AVG(salary),1) desc;
 
 /*
 문제 10.
@@ -112,12 +112,12 @@ ORDER BY AVG(salary) desc;
 #5 optDate 컬럼 데이터로 출력
 #6 입사일 오름차순 정렬
 */
-SELECT  first_name,
-        hire_date,
+SELECT  first_name 이름,
+        hire_date 입사일,
         CASE    WHEN hire_date <= '02.12.31'    THEN '창립맴버'
                 WHEN hire_date BETWEEN '03.01.01' and '03.12.31'    THEN '03년 입사'
                 WHEN hire_date BETWEEN '04.01.01' and '04.12.31'    THEN '04년 입사'
                 ELSE '상장 이후 입사'
         end "optDate"       
 FROM    employees
-ORDER BY hire_date asc;
+ORDER BY 입사일 asc;-- >> ORDER BY hire_date asc;
