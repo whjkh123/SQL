e90f2149e1f398fec5e629212e5e68dcd392f8a5
whjkh <whjkh@DESKTOP-O9T8HTK>
2020-12-21 19:21:37 +0900

    #2020. 12. 21 수업과제 #LEFT/RIGHT OUTER JOIN #INNER JOIN

--- Practice/201221_practice03.sql
+++ Practice/201221_practice03.sql
@@ -183,8 +183,17 @@ WHERE   e.department_id = d.department_id(+)
                                                                                     부서명을 불러오기 위해 departments를 'd'로 정의해 JOIN
 #2 e.manager_id = m.employee_id >> 첫번째 시도 LEFT OUTER JOIN employees m
                                                 ON e.manager_id = m.employee_id >> employees를 'e'로 정의해 기준으로 삼고 SELF JOIN해 각 employee_id 데이터를 manger_id에 대입하여 관리자 산출
-                                    실패 >> employees를 SELF JOIN 함으로써 manager_id 데이터는 갖지않지만(정확히는 NULL 데이터를 가졌다.), employees.employee_id 데이터를 가진 'Steven' 출력
+                                    실패 >> employees를 SELF JOIN 함으로써 manager_id 데이터는 갖지않지만(정확히는 NULL 데이터를 가졌다.), employee_id 데이터를 가진 'Steven' 출력
+                                            FROM A
+                                            LEFT OUTER JOIN B
+                                            ON A.depth = B.depth
+                                            >> A ∪ B(A와 B의 합집합) 산출(employee_id, manager_id)
                                 >> 두번째 시도 INNER JOIN employees m
                                                 ON e.manager_id = m.employee_id ↔ WHERE e.manager_id = m.employee_id WHERE문을 풀어보면, e.manager_id와 m.employee_id가 같으면 출력, 나머지 생략 즉, 교집합을 검사/산출
-                                                따라서, INNER JOIN 함으로써 e.manager_id 데이터와 m.employee_id 데이터 모두 갖는 row 값을 산출 >> manager_id 데이터를 갖지않은 'Steven' 생략 성공            
+                                                따라서, INNER JOIN 함으로써 e.manager_id 데이터와 m.employee_id 데이터 모두 갖는 row 값을 산출 >> manager_id 데이터를 갖지않은 'Steven' 생략
+                                    성공
+                                            FROM A
+                                            INNER JOIN B
+                                            ON A.depth = B.depth
+                                            >> A ∩ B(A와 B의 교집합) 산출(employee_id)
 */
